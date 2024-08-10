78ff6be47b42c92b8fe6f1990a97976a85428ad4
Aritra Bera <aritrabera69@gmail.com>
2024-08-10 14:50:15 +0530

    2nd commit

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="flex justify-center items-center h-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
+      {/* Main  */}
+      <main>
+        {/* Top  */}
+        <div className="flex justify-center items-center space-x-4 mb-3">
+          <img className="w-16 lg:w-28" src="https://www.devknus.com/img/react.png" alt="react logo" />
+          <img className="w-8 lg:w-10" src="https://cdn-icons-png.flaticon.com/128/10238/10238173.png" alt="" />
+          <img className="w-40 lg:w-72" src="razorpay.png" alt="razorpay logo" />
+        </div>
+
+        {/* Bottom  */}
+        <h1 className=" text-xl text-center lg:text-3xl font-bold">
+          Razorpay Payment Integration using React And Node js
+        </h1>
+      </main>
+    </section>
+  );
 }
 
-export default App
+export default App;
