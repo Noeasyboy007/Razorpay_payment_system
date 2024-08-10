d4a0859d2ece38373429b603e06d72f90bba13d3
Aritra Bera <aritrabera69@gmail.com>
2024-08-11 02:45:43 +0530

    final ccommit

--- frontend/vite-project/src/components/ProductCard.jsx
+++ frontend/vite-project/src/components/ProductCard.jsx
@@ -80,7 +80,7 @@ export default function ProductCard() {
             <CardHeader color="" className="relative h-96 bg-[#2C3A47]">
                 {/* Image  */}
                 <img
-                    src="../../public/product.png"
+                    src="https://i.pinimg.com/564x/bd/70/60/bd7060e636365d36c2cf278cd1bcb85c.jpg"
                     alt="card-image"
                 />
             </CardHeader>
